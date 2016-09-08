9259db379bc46c30ae01295f8bc2116f6a643241
Lee Jordan <ldjordan@gmail.com>
2016-09-08 17:53:38 +0100

    handle youtube api errors better

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
 
         $httpHost = $_SERVER['HTTP_HOST'];
         $httpRequest = $_SERVER['REQUEST_URI'];
-
         $request = "https://www.googleapis.com/youtube/v3/videos/?chart=mostPopular&maxResults=50&part=id,snippet&key=AIzaSyAqC1qAEVYz-iTZbS1uPGLcI2Vrk3lHX2k";
         $requestOptions = array(
             'http'=>array(
@@ -26,28 +25,38 @@
             )
         );
         $requestStream = stream_context_create($requestOptions);
-
         $rawJson = @file_get_contents($request, false, $requestStream);
-
-        if ($rawJson === FALSE) {
-            echo '<div class="error"><h2>Oh Dear</h2><p>I had some problems contacting youtube for video information and I am now sulking. I probably hit my limit for youtube API requests. You can <a href="https://www.youtube.com/">go to the real youtube</a> if you want to but be careful - it can be addictive.</p></div>';
-            exit;
-        }
-
-        $decodedJson = json_decode($rawJson);
         $formattedResults = new stdClass();
         $count = 0;
 
-        foreach($decodedJson->items as $item) {
-            $formattedResults->$count = [
-                'id' => $item->id,
-                'title' => $item->snippet->title,
-            ];
-            $count++;
+        // handle the possibility that the youtube api might reach it's limits or go down - fall back to local data
+        if ($rawJson === FALSE) {
+            $rawJson = @file_get_contents("data/php-generated-data.json");
+            $decodedJson = json_decode($rawJson);
+            foreach($decodedJson as $item) {
+                $formattedResults->$count = [
+                    'id' => $item->id,
+                    'title' => $item->title,
+                ];
+                $count++;
+            }
+            echo '<div class="error"><p>I had some problems contacting youtube for current video information and I am forced to use the last set of videos that I can remember. Sorry about that!</div>';
+        } else {
+            $decodedJson = json_decode($rawJson);
+            foreach($decodedJson->items as $item) {
+                $formattedResults->$count = [
+                    'id' => $item->id,
+                    'title' => $item->snippet->title,
+                ];
+                $count++;
+            }
         }
 
-        $initialVideoCount = 1;
+        // generates a local backup in case youtube api is down or limit is reached
+        // file_put_contents("data/php-generated-data.json", json_encode($formattedResults));
+
         echo '<ol id="video-list" class="list">';
+        $initialVideoCount = 1;
         foreach($formattedResults as $video) {
             echo '<li class="list__vid"><a href="https://www.youtube.com/watch?v=' . $video['id'] . '" data-id="' . $video['id'] . '" onclick="return openModal(\'' . $video['id'] . '\')"><h3>' . $video['title'] . '</h3></a></li>';
             if ($initialVideoCount == 10) { break; }
